a7b384bfe3a95a3643952347960d0e4456b8d061
Tyler Peterson <tylermpeterson36@gmail.com>
2017-09-18 11:45:42 -0600

    recent updates

--- setMonitors.sh
+++ setMonitors.sh
@@ -1,4 +1,9 @@
-# monitor 1
-xrandr --newmode "2560x1080_60.00"  230.76  2560 2728 3000 3440  1080 1081 1084 1118  -HSync +Vsync
-xrandr --addmode HDMI1 "2560x1080_60.00"
-xrandr --output HDMI1 --mode "2560x1080_60.00"
+# vertical monitor
+xrandr --newmode "vertical"  178.35  1080 1168 1288 1496  1920 1921 1924 1987  -HSync +Vsync
+xrandr --addmode HDMI2 "vertical"
+xrandr --output HDMI2 --mode "vertical"
+
+# ultrawide
+xrandr --newmode "ultrawide"  230.76  2560 2728 3000 3440  1080 1081 1084 1118  -HSync +Vsync
+xrandr --addmode HDMI1 "ultrawide"
+xrandr --output HDMI1 --mode "ultrawide"
