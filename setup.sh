0ddbca6d03471c1d05b65fc84c198ace0b9c0603
Tyler Peterson <tylermpeterson36@gmail.com>
2017-11-08 19:14:03 -0700

    changed setup to after prezto setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,4 @@
-# run this file after installing zsh.
-
-## installing & configuring zprezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto";
-
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+# run this file after installing zsh and prezto
 
 rm -f ~/.zshrc;
 rm -f ~/.zpreztorc;
@@ -25,6 +17,23 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 # General server
 sudo apt-get install fail2ban;
+
+sudo rm -f /etc/apt/apt.conf.d/10periodic;
+sudo rm -f /etc/apt/apt.conf.d/50unattended-upgrades;
+
+sudo echo "APT::Periodic::Update-Package-Lists '1';
+APT::Periodic::Download-Upgradeable-Packages '1';
+APT::Periodic::AutocleanInterval '7';
+APT::Periodic::Unattended-Upgrade '1';" > /etc/apt/apt.conf.d/10periodic;
+
+
+sudo echo "Unattended-Upgrade::Allowed-Origins {
+        '${distro_id}:${distro_codename}';
+        '${distro_id}:${distro_codename}-security';
+        'Ubuntu lucid-security';
+    //  'Ubuntu lucid-updates';
+};" > /etc/apt/apt.conf.d/50unattended-upgrades;
+
 
 sudo apt-get install logwatch
 sudo echo "/usr/sbin/logwatch --output mail --mailto tylermpeterson36@gmail.com --detail high" >> /etc/cron.daily/00logwatch;
