b2785c45ffe63ce34ddc2c448038d394c8658ac2
Tyler Peterson <tylermpeterson36@gmail.com>
2017-11-08 19:42:14 -0700

    more updates

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 # run this file after installing zsh and prezto
-ln -s /$USER/dotfiles/.gitconfig /$USER/.gitconfig
-
+ln -s /$USER/dotfiles/.gitconfig /$USER/.gitconfig;
 
 rm -f ~/.zshrc;
 rm -f ~/.zpreztorc;
@@ -8,40 +7,20 @@ rm -f ~/.zpreztorc;
 ln -s /$USER/dotfiles/zsh/.zshrc /$USER/.zshrc;
 ln -s /$USER/dotfiles/zsh/.zpreztorc /$USER/.zpreztorc;
 
-
-source ~/.zshrc
-
 # Vim-plug
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
 
+ln -s /$USER/dotfiles/vim/.vimrc /$USER/.vimrc;
 
-ln -s /$USER/dotfiles/vim/.vimrc /$USER/.vimrc
 # General server
 sudo apt-get install fail2ban;
 
-sudo rm -f /etc/apt/apt.conf.d/10periodic;
-sudo rm -f /etc/apt/apt.conf.d/50unattended-upgrades;
-
-sudo echo "APT::Periodic::Update-Package-Lists '1';
-APT::Periodic::Download-Upgradeable-Packages '1';
-APT::Periodic::AutocleanInterval '7';
-APT::Periodic::Unattended-Upgrade '1';" > /etc/apt/apt.conf.d/10periodic;
-
-
-sudo echo "Unattended-Upgrade::Allowed-Origins {
-        '${distro_id}:${distro_codename}';
-        '${distro_id}:${distro_codename}-security';
-        'Ubuntu lucid-security';
-    //  'Ubuntu lucid-updates';
-};" > /etc/apt/apt.conf.d/50unattended-upgrades;
-
-
-sudo apt-get install logwatch
+sudo apt-get install logwatch;
 sudo echo "/usr/sbin/logwatch --output mail --mailto tylermpeterson36@gmail.com --detail high" >> /etc/cron.daily/00logwatch;
-
-
 
 sudo apt-get update -y;
 sudo apt-get upgrade -y;
+
+source ~/.zshrc;
