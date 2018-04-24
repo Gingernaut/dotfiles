009557716016e481898e53bc266d4a393cf2ac76
Tyler Peterson <tylermpeterson36@gmail.com>
2018-04-24 16:00:51 -0700

    updated work settings

--- pullAll.sh
+++ pullAll.sh
@@ -6,8 +6,9 @@ cd ~/Code/monolith/language; git pull origin master; pwd; echo "\n -------------
 
 # Streaming
 cd ~/Code/streaming/ap-stream; git pull origin master; pwd; echo "\n --------------------- \n";
+cd ~/Code/streaming/ap-stream-lib; git pull origin master; pwd; echo "\n --------------------- \n";
 cd ~/Code/streaming/ap-stream-ingress; git pull origin master; pwd; echo "\n --------------------- \n";
 cd ~/Code/streaming/ap-stream-egress; git pull origin master; pwd; echo "\n --------------------- \n";
 
 # General
-cd ~/Code/ae-docker-images; git pull origin master; pwd; echo "\n --------------------- \n";
+cd ~/Code/docker-images; git pull origin master; pwd; echo "\n --------------------- \n";
